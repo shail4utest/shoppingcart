b7858934aaea3324dfda8e7662a32306bca766d6
Shailesh Patil <shaileshppatil@yahoo.com>
2017-03-18 14:14:51 +0000

    Added offer 3for2

--- src/main/scala/store/offers/Offers.scala
+++ src/main/scala/store/offers/Offers.scala
@@ -7,6 +7,8 @@ trait Offers {
 
   def buyOneGetOneFree(quantity: Int, price: BigDecimal): BigDecimal = ( (quantity / 2) + (quantity % 2)) * price
 
+  def threeForTwo(quantity: Int, price: BigDecimal): BigDecimal = (2 * (quantity/3) + (quantity %3)) * price
+
 }
 
 object Offers extends Offers
--- src/test/scala/store/offers/OffersSpec.scala
+++ src/test/scala/store/offers/OffersSpec.scala
@@ -11,4 +11,11 @@ class OffersSpec  extends FlatSpec with Matchers {
     Offers.buyOneGetOneFree(2 ,0.60) should be (0.60)
   }
 
+  "4 items" should "cost 1.20 when price for 1 is 0.60" in {
+    Offers.buyOneGetOneFree(4 ,0.60) should be (1.20)
+  }
+
+  "3 items" should "cost 1.20 when price for 1 is 0.60" in {
+    Offers.threeForTwo(3 ,0.60) should be (1.20)
+  }
 }
