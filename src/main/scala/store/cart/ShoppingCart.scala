8763593f8768692005404598da06db134c065c93
Shailesh Patil <shaileshppatil@yahoo.com>
2017-03-18 14:21:54 +0000

    Updated the shopping cart with offers on Apples

--- src/main/scala/store/cart/ShoppingCart.scala
+++ src/main/scala/store/cart/ShoppingCart.scala
@@ -1,12 +1,20 @@
 package store.cart
 
+import store.offers.Offers
+
 /**
   * Created by shailesh on 18/03/2017.
   */
-trait ShoppingCart {
+trait ShoppingCart extends Offers{
   def checkout(items:Array[String]) = {
 
     items.count(_.equalsIgnoreCase("Apple")) * 0.60 + items.count(_.equalsIgnoreCase("Orange")) * 0.25
+
+  }
+
+  def checkoutWithOffers(items:Array[String]) = {
+
+    buyOneGetOneFree(items.count(_.equalsIgnoreCase("Apple")) , 0.60 )
 
   }
 
