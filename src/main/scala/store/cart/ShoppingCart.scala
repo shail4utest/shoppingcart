47d3c221b68b5040ab30ac8d48d42adfa5f88e32
Shailesh Patil <shaileshppatil@yahoo.com>
2017-03-18 16:32:13 +0000

    Refactored and move the item price to seprate price feed service

--- src/main/scala/store/cart/ShoppingCart.scala
+++ src/main/scala/store/cart/ShoppingCart.scala
@@ -1,24 +1,26 @@
 package store.cart
 
 import store.offers.Offers
+import store.price.PriceService
 
 /**
   * Created by shailesh on 18/03/2017.
   */
 trait ShoppingCart extends Offers{
 
+  val priceService = PriceService
 
   def checkout(items:Array[String]) = {
 
-    items.count(_.equalsIgnoreCase("Apple")) * 0.60 +
-      items.count(_.equalsIgnoreCase("Orange")) * 0.25
+      items.count(_.equalsIgnoreCase("Apple")) * priceService.getItemPrice("Apple").getOrElse(BigDecimal(0.0)) +
+        items.count(_.equalsIgnoreCase("Orange")) * priceService.getItemPrice("Orange").getOrElse(BigDecimal(0.0))
 
   }
 
   def checkoutWithOffers(items:Array[String]) = {
 
-    buyOneGetOneFree(items.count(_.equalsIgnoreCase("Apple")) , 0.60 ) +
-      threeForTwo(items.count(_.equalsIgnoreCase("Orange")) ,0.25)
+    buyOneGetOneFree(items.count(_.equalsIgnoreCase("Apple")) ,priceService.getItemPrice("Apple").getOrElse(BigDecimal(0.0))) +
+      threeForTwo(items.count(_.equalsIgnoreCase("Orange")) ,priceService.getItemPrice("Orange").getOrElse(BigDecimal(0.0)))
 
   }
 
